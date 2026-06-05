b6e47130d3eb5243e8e93cb39460270c250145a2
juliescalvin1800-a11y <juliescalvin1800@gmail.com>
2026-06-05 14:08:37 +0530

    Update live.sh

--- live.sh
+++ live.sh
@@ -3,7 +3,7 @@
 # Tamil Christian Songs 24/7 YouTube Live Stream
 # Songs from: https://archive.org/details/09.-aaviai-malaipola
 
-STREAM_KEY="rtxv-yegk-yvsd-djcg-0kvm"
+STREAM_KEY="YOUR_YOUTUBE_STREAM_KEY_HERE"
 PLAYLIST="playlist.txt"
 BACKGROUND="video.mp4"
 
@@ -11,25 +11,29 @@ while true; do
   # Shuffle playlist each loop
   shuf "$PLAYLIST" | while read -r AUDIO_URL; do
     echo "Now playing: $AUDIO_URL"
-    ffmpeg -re -stream_loop -1 -i "$BACKGROUND" \
+    ffmpeg -re \
+           -stream_loop -1 -i "$BACKGROUND" \
            -i "$AUDIO_URL" \
            -map 0:v -map 1:a \
            -vcodec libx264 \
-           -pix_fmt yuvj420p \
-           -maxrate 2000k \
-           -preset veryfast \
-           -r 30 \
-           -g 60 \
+           -pix_fmt yuv420p \
+           -preset ultrafast \
+           -tune zerolatency \
+           -r 24 \
+           -g 48 \
+           -s 854x480 \
+           -b:v 800k \
+           -maxrate 1000k \
+           -bufsize 2000k \
            -c:a aac \
            -b:a 128k \
            -ar 44100 \
+           -ac 2 \
            -strict experimental \
            -video_track_timescale 1000 \
-           -s 1280x720 \
-           -b:v 1500k \
            -f flv \
-           -ac 2 \
            "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY" \
            2>/dev/null || true
+    sleep 1
   done
 done
